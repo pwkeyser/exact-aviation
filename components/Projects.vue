3cbe86f988382aa145c3e168cff451ad2a81c924
Wouter Keyser <wouter.keyser@credibilium.com>
2020-10-06 17:24:00 +0200

    Added latest map coordinates

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,20 +1,15 @@
 <template>
-  <div id="projects">
-    <div class="NavBarGap GreyColour" />
-    <b-jumbotron :header="headerText" class="NoPaddingMargins">
-      <div class="pre-formatted">
-        {{ descriptionText }}
-      </div>
-      <gmap-map :center="center" :map-type-id="mapTypeId" :zoom="2">
-        <gmap-marker
-          v-for="(item, index) in markers"
-          :key="index"
-          :position="item.position"
-          @click="center = item.position"
-        />
-      </gmap-map>
-    </b-jumbotron>
-  </div>
+<div id="projects">
+  <div class="NavBarGap GreyColour" />
+  <b-jumbotron :header="headerText" class="NoPaddingMargins">
+    <div class="pre-formatted">
+      {{ descriptionText }}
+    </div>
+    <gmap-map :center="center" :map-type-id="mapTypeId" :zoom="2">
+      <gmap-marker v-for="(item, index) in markers" :key="index" :position="item.position" @click="center = item.position" />
+    </gmap-map>
+  </b-jumbotron>
+</div>
 </template>
 
 <script>
@@ -25,16 +20,47 @@ export default {
     return {
       headerText: 'Current Projects',
       descriptionText: '',
-      center: { lat: -25.8645677, lng: 28.2409667 },
+      center: {
+        lat: -25.8645677,
+        lng: 28.2409667
+      },
       mapTypeId: 'terrain',
-      markers: [
-        { position: { lat: -19.9758282, lng: 23.425978 } },
-        { position: { lat: -17.8305953, lng: 25.1615299 } },
-        { position: { lat: -27.3942096, lng: 153.1196416 } },
-        { position: { lat: -13.8304448, lng: -172.0102986 } },
-        { position: { lat: 42.5745676, lng: 21.0273197 } },
-        { position: { lat: 19.114488, lng: 50.1229026 } },
-        { position: { lat: -10.6875155, lng: 40.2206478 } },
+      markers: [{
+          position: {
+            lat: 42.5745676,
+            lng: 21.0273197
+          }
+        },
+        {
+          position: {
+            lat: -18.0459206,
+            lng: 178.5578667
+          }
+        },
+        {
+          position: {
+            lat: 0.0422892,
+            lng: 32.4346266
+          }
+        },
+        {
+          position: {
+            lat: -34.0043598,
+            lng: 22.378785
+          }
+        },
+        {
+          position: {
+            lat: -4.1044393,
+            lng: 16.4745968
+          }
+        },
+        {
+          position: {
+            lat: 36.5308882,
+            lng: 2.0916757
+          }
+        },
       ],
     }
   },
